ffe1dfff9aca47998fc87d870df62ee1afb8931a
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-27 20:40:46 +0100

    Add component for stats

---
+++ frontend/src/components/CutchaStats/CutchaStats.tsx
@@ -0,0 +1,22 @@
+import './CutchaStats.css';
+
+import React from 'react';
+import { PuzzleTypeCount } from '../../types/puzzle';
+
+interface CutchaStatsProps {
+    stats: PuzzleTypeCount[];
+}
+
+function CutchaStats({ stats }: CutchaStatsProps): JSX.Element {
+    return (
+        <div className="CutchaStats">
+            {stats.map(({ _id, typCount }) => (
+                <p key={_id}>
+                    {typCount} {_id}
+                </p>
+            ))}
+        </div>
+    );
+}
+
+export default CutchaStats;
