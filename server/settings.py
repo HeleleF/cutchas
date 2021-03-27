ae75632df19575d2b89444f13db1a8092d973621
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-27 21:35:55 +0100

    Remove everything that wont be deployed

--- server/settings.py
+++
@@ -1,9 +0,0 @@
-# Flask settings
-FLASK_SERVER_NAME = 'localhost:7331'
-FLASK_DEBUG = True  # Do not use debug mode in production
-
-# Flask-RestX settings
-
-# SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = 'sqlite:///cutcha.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
