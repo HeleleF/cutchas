8aea960e63f584dd23be968cc4805362ca0021af
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-28 13:31:43 +0200

    Use real user agent

--- backend/src/util/axios.ts
+++ backend/src/util/axios.ts
@@ -5,7 +5,8 @@ const axiosInstance = axios.create({
     headers: {
         Origin: 'https://cutcaptcha.com',
         Referer: `${SECRETS.CUTCHA_API_URL}.html`,
-        'User-Agent': 'Mozilla/5.0',
+        'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 OPR/74.0.3911.232',
         'X-Requested-With': 'XMLHttpRequest',
         'Content-Type': 'application/json; charset=UTF-8',
     },
