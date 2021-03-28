d03ffdf23c4afe608284d0cc5d26b5999aff1daf
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-03-28 13:29:08 +0200

    Use api url with template

--- backend/src/util/axios.ts
+++ backend/src/util/axios.ts
@@ -1,14 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
+import { SECRETS } from './secrets';
 
 const axiosInstance = axios.create({
-  headers: {
-    Origin: "https://cutcaptcha.com",
-    Referer: "https://cutcaptcha.com/captcha/SAs61IAI.html",
-    "User-Agent": "Mozilla/5.0",
-    "X-Requested-With": "XMLHttpRequest",
-    "Content-Type": "application/json; charset=UTF-8",
-  },
-  timeout: 10000,
+    headers: {
+        Origin: 'https://cutcaptcha.com',
+        Referer: `${SECRETS.CUTCHA_API_URL}.html`,
+        'User-Agent': 'Mozilla/5.0',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json; charset=UTF-8',
+    },
+    timeout: 10000,
 });
 
 export default axiosInstance;
