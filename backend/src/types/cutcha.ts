6eadb548fc1e1bb68c2b80a1c3d991ea60c090d8
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-04-03 17:22:19 +0200

    Run prettier

--- backend/src/types/cutcha.ts
+++ backend/src/types/cutcha.ts
@@ -1,20 +1,20 @@
 export interface CutchaPuzzle {
-  succ: boolean;
-  captcha_question: string;
-  captcha_token: string;
+    succ: boolean;
+    captcha_question: string;
+    captcha_token: string;
 }
 
 export interface CutchaPuzzleSuccess {
-  succ: true;
-  correct: boolean;
+    succ: true;
+    correct: boolean;
 }
 
 export interface CutchaPuzzleNoSuccess {
-  succ: false;
-  err: "captcha_token is invalid or expired";
+    succ: false;
+    err: 'captcha_token is invalid or expired';
 }
 
 export type CutchaPuzzleSubmitResult =
-  | CutchaPuzzleSuccess
-  | CutchaPuzzleNoSuccess
-  | "captcha_token is missing";
+    | CutchaPuzzleSuccess
+    | CutchaPuzzleNoSuccess
+    | 'captcha_token is missing';
