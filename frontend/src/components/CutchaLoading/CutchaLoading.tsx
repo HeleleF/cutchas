d3344d9fae3086b9fb5af24e87246603685b8461
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-04-12 18:21:29 +0200

    Add loading animation for puzzle pieces
    We pass scale as props so resizing while loading should work

--- frontend/src/components/CutchaLoading/CutchaLoading.tsx
+++ frontend/src/components/CutchaLoading/CutchaLoading.tsx
@@ -1,9 +1,21 @@
 import './CutchaLoading.css';
 
-function CutchaLoading(): JSX.Element {
+function CutchaLoading({ scale }: { scale: number }): JSX.Element {
     return (
         <div className="CutchaLoading" title="Loading...">
-            <span className="CutchaLoadingMessage">Loading new puzzle, please wait...</span>
+            <div className="DragAreaStub">
+                <div className="StartAreaStub">
+                    <span className="PuzzlePieceStub"></span>
+                    <span className="PuzzlePieceStub"></span>
+                    <span className="PuzzlePieceStub"></span>
+                </div>
+                <span
+                    className="PuzzleMainStub"
+                    style={{ width: Math.ceil(scale * 448), height: Math.ceil(scale * 332) }}
+                ></span>
+            </div>
+
+            <span className="SubmitStub"></span>
         </div>
     );
 }
