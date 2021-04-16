2f4ffb683c7a59d2ab77cf14c4383809d43347cc
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-04-16 16:55:34 +0200

    Use cutcha utils module for cutcha handling

--- backend/src/controllers/puzzleController.ts
+++ backend/src/controllers/puzzleController.ts
@@ -1,17 +1,7 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { Puzzle } from '../models/Puzzle.js';
-import { CutchaApiResult, CutchaPuzzleSubmitResult } from '../types/cutcha.js';
-import axiosInstance, { loadParts } from '../util/axios.js';
-import { SECRETS } from '../util/secrets.js';
-
-const payload = {
-    f: '',
-    i: '4256A7a1XEiEL72IUdPL97ei7EuEEAuu',
-    ip: 0,
-    papi:
-        'ObSji8QE5hJIU947p50ySuF3isUIO4KswlERKBm/oLgbFujikI1G0dg04SJWCcs5f73VHChCnS+DbMuG5fudZUVBiQkDLq8vb3L/dHY8QacOSQSr6raGu8CaiQvBxwithYvYDiqUwiL+e2CeC5EF85kb4upaLJXTuJ8kUm/tZeZSQIoaeHelLjk4QNXhtt/WRS/0agKvytpoehx444gMz3R9EUhXbUk+PNrsazYDqmlIShMDq+6M+xBarz2PMtL5f8Vv7YYvuvoNPOizPtmmKht9ST+AVEJe/kY5B0GC7b+QEX3VFgR5vpGBQHt9ohw4SyCVAkX4I56wt2NdcaeO/7PjkZNtZrqO4hhs6SYZjc7pa2mj786V7A==',
-};
+import { loadParts, requestNewCutcha, validateCutcha } from '../util/cutcha.js';
 
 export const getPuzzle = async (_: Request, res: Response): Promise<void> => {
     // repeat until we get a puzzle thats either UNSOLVED or NEW
@@ -21,38 +11,26 @@ export const getPuzzle = async (_: Request, res: Response): Promise<void> => {
     let tries = 10;
 
     do {
-        const { data } = await axiosInstance.post<CutchaApiResult>(
-            `${SECRETS.CUTCHA_API_URL}/${SECRETS.CUTCHA_API_KEY}.json`,
-            {
-                api_key: SECRETS.CUTCHA_API_KEY,
-            },
-        );
-
-        if ('string' === typeof data) {
-            res.status(500).json({
-                error: `Expected json, but recieved ${data}`,
-            });
+        const maybeCutcha = await requestNewCutcha();
+        if (!maybeCutcha) {
+            res.status(500).json({ error: 'Failed to get a new cutcha' });
             return;
         }
+        const { captcha_question, captcha_token } = maybeCutcha;
 
-        if (!data.succ) {
-            res.status(500).json(data);
-            return;
-        }
-
-        console.log(`Recieved new puzzle: ${data.captcha_question}`);
+        console.log(`Recieved new puzzle: ${captcha_question}`);
         const maybeSolved = await Puzzle.findOne({
-            question: data.captcha_question,
+            question: captcha_question,
             typ: 'solved',
         });
         if (maybeSolved) continue;
 
-        const images = await loadParts(data.captcha_question);
+        const images = await loadParts(captcha_question);
         if (!images) continue; // TODO(helene): mark as broken while we're here
 
         res.status(200).json({
-            id: data.captcha_question,
-            token: data.captcha_token,
+            id: captcha_question,
+            token: captcha_token,
             images,
         });
         return;
@@ -75,31 +53,9 @@ export const submitPuzzle = async (
 
     const { id, token, x0, x1, x2, y0, y1, y2 } = req.body;
 
-    const { data } = await axiosInstance.post<CutchaPuzzleSubmitResult>(
-        `${SECRETS.CUTCHA_API_URL}/${SECRETS.CUTCHA_API_KEY}/check`,
-        {
-            ...payload,
-            captcha_token: token,
-            solution: [
-                [x0, y0, 0],
-                [x1, y1, 0],
-                [x2, y2, 0],
-            ],
-        },
-    );
-
-    if ('string' === typeof data) {
-        res.status(422).json({ error: data });
-        return;
-    }
-
-    if (!data.succ) {
-        res.status(422).json({ error: data.err });
-        return;
-    }
-
-    if (!data.correct) {
-        res.status(422).json({ error: 'not correct' });
+    const valid = await validateCutcha(token, { x0, x1, x2, y0, y1, y2 });
+    if (!valid) {
+        res.status(500).json({ error: 'Faild to validate token' });
         return;
     }
 
--- backend/src/controllers/tokenController.ts
+++ backend/src/controllers/tokenController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
 import { Puzzle } from '../models/Puzzle.js';
-import { CutchaApiResult, CutchaPuzzleSubmitResult } from '../types/cutcha.js';
-import axiosInstance from '../util/axios.js';
-import { CUTCHA_SUBMIT_PAYLOAD } from '../util/cutcha.js';
+import { requestNewCutcha, validateCutcha } from '../util/cutcha.js';
 import { SECRETS } from '../util/secrets.js';
 
 export const validate = async (req: Request, res: Response): Promise<void> => {
@@ -14,27 +12,15 @@ export const validate = async (req: Request, res: Response): Promise<void> => {
         return;
     }
 
-    const { data } = await axiosInstance.post<CutchaApiResult>(
-        `${SECRETS.CUTCHA_API_URL}/${SECRETS.CUTCHA_API_KEY}.json`,
-        {
-            api_key: SECRETS.CUTCHA_API_KEY,
-        },
-    );
-
-    if ('string' === typeof data) {
-        res.status(500).json({
-            error: `Expected json, but recieved ${data}`,
-        });
+    // 1. Request a new puzzle
+    const maybeCutcha = await requestNewCutcha();
+    if (!maybeCutcha) {
+        res.status(500).json({ error: 'Failed to get a new cutcha' });
         return;
     }
+    const { captcha_question, captcha_token } = maybeCutcha;
 
-    if (!data.succ) {
-        res.status(500).json(data);
-        return;
-    }
-
-    const { captcha_question, captcha_token } = data;
-
+    // 2. Look up solution in our database
     const solution = await Puzzle.findOne({
         question: captcha_question,
         typ: 'solved',
@@ -45,35 +31,13 @@ export const validate = async (req: Request, res: Response): Promise<void> => {
         return;
     }
 
-    const { x0, x1, x2, y0, y1, y2 } = solution;
-
-    const { data: result } = await axiosInstance.post<CutchaPuzzleSubmitResult>(
-        `${SECRETS.CUTCHA_API_URL}/${SECRETS.CUTCHA_API_KEY}/check`,
-        {
-            ...CUTCHA_SUBMIT_PAYLOAD,
-            captcha_token,
-            solution: [
-                [x0, y0, 0],
-                [x1, y1, 0],
-                [x2, y2, 0],
-            ],
-        },
-    );
-
-    if ('string' === typeof result) {
-        res.status(500).json({ error: result });
+    // 3. Validate token by submitting our solution
+    const valid = await validateCutcha(captcha_token, solution);
+    if (!valid) {
+        res.status(500).json({ error: 'Faild to validate token' });
         return;
     }
 
-    if (!result.succ) {
-        res.status(500).json({ error: result.err });
-        return;
-    }
-
-    if (!result.correct) {
-        res.status(500).json({ error: 'not correct' });
-        return;
-    }
-
+    // 4. Token is now valid for two minutes
     res.status(200).json({ token: captcha_token });
 };
