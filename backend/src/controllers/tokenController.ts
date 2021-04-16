07d5b3bf335ddf9c5d034e6acc8b0a4639503834
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-04-16 15:50:03 +0200

    Protect endpoint with custom token

--- backend/src/controllers/tokenController.ts
+++ backend/src/controllers/tokenController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 
-export const validate = async (_: Request, res: Response): Promise<void> => {
-    res.json({});
+export const validate = async (req: Request, res: Response): Promise<void> => {
+    if (!req.header('apiKey') || req.header('apiKey') !== process.env.API_KEY) {
+        res.status(401).json({ status: 'error', message: 'Unauthorized.' });
+        return;
+    }
+    res.status(200).json({});
 };
