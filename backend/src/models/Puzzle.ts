c1542a2f9a76a25fcffedf12daaad625f56106b5
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-04-16 16:54:43 +0200

    Update puzzle document type

--- backend/src/models/Puzzle.ts
+++ backend/src/models/Puzzle.ts
@@ -7,14 +7,14 @@ export type PuzzleDocument = mongoose.Document & {
 
     typ: PuzzleTyp;
 
-    x0?: number;
-    y0?: number;
+    x0: number;
+    y0: number;
 
-    x1?: number;
-    y1?: number;
+    x1: number;
+    y1: number;
 
-    x2?: number;
-    y2?: number;
+    x2: number;
+    y2: number;
 };
 
 const puzzleSchema = new mongoose.Schema<PuzzleDocument>(
